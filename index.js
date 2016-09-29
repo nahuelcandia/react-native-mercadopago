fde874ca983123a69d3980cc0470a8771e2e6e95
Samuel Chalela Naffah <samuel.chalelanaffah@crownresorts.com.au>
2016-09-29 23:04:54 +1000

    Refactor index

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ import { NativeModules } from 'react-native';
 
 const { RNMercadopago } = NativeModules;
 
-export default RNMercadopago;
+export default {
+  startCheckout: function(publicKey, prefId, color, blackFont, success, failure) {
+    RNMercadopago.startCheckout([publicKey, prefId, color, blackFont], success, failure);
+  }
+};
