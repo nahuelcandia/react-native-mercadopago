79f457205ec5f33a2a57c92320e6e9eb7da026ed
Samuel Chalela Naffah <samuel.chalelanaffah@crownresorts.com.au>
2016-10-09 11:54:00 +1100

    Update SDK

---
+++ ios/MPGoogleAnalytics.framework/Headers/MPGoogleAnalytics-umbrella.h
@@ -0,0 +1,16 @@
+#import <UIKit/UIKit.h>
+
+#import "GAI.h"
+#import "GAIDictionaryBuilder.h"
+#import "GAIEcommerceFields.h"
+#import "GAIEcommerceProduct.h"
+#import "GAIEcommerceProductAction.h"
+#import "GAIEcommercePromotion.h"
+#import "GAIFields.h"
+#import "GAILogger.h"
+#import "GAITrackedViewController.h"
+#import "GAITracker.h"
+
+FOUNDATION_EXPORT double MPGoogleAnalyticsVersionNumber;
+FOUNDATION_EXPORT const unsigned char MPGoogleAnalyticsVersionString[];
+
---
+++ ios/MercadoPagoTracker.framework/Headers/MercadoPagoTracker-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "MPTracker.h"
+
+FOUNDATION_EXPORT double MercadoPagoTrackerVersionNumber;
+FOUNDATION_EXPORT const unsigned char MercadoPagoTrackerVersionString[];
+
